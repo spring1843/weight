69373232f233cd610e67a45e6fa5127a140afb7a
Yasser Abdolmaleki <yasser@yasser.ca>
2020-08-10 00:39:05 -0700

    Refactor, reuse in cpu readers

--- cpu_lin.go
+++ cpu_lin.go
@@ -1,30 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os/exec"
-	"strconv"
-	"strings"
 )
 
-// linuxReader can read CPU load on Linux
-type linuxReader struct {
-	path string
-}
-
-func newLinuxReader() (*linuxReader, error) {
-	path, err := exec.LookPath(topCmd)
-	if err != nil {
-		return nil, fmt.Errorf("failed finding the location of the top command. Error : %w", err)
+func readLinuxCPULoad() (float32, error) {
+	if topPath == "" {
+		return 0, errors.New("empty top path")
 	}
 
-	return &linuxReader{
-		path: path,
-	}, nil
-}
-
-func (o *linuxReader) read() (float32, error) {
-	cmd := exec.Command(o.path, []string{"-bn1"}...)
+	cmd := exec.Command(topPath, []string{"-bn1"}...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return -1, fmt.Errorf("failed to execute the top command. Error : %w", err)
@@ -32,43 +19,12 @@ func (o *linuxReader) read() (float32, error) {
 	return parseLinuxTopOutput(string(out))
 }
 
-// parseLinuxTopOutput parses the output of top command looking for CPU load
-// The load is calculated by finding the idle percentage and then subtracting it from 100
+// parseLinuxTopOutput parses the output of top command in linux looking for CPU load
 func parseLinuxTopOutput(topOutput string) (float32, error) {
-	const (
-		keywordCPULoad = "%Cpu(s):"
-		keywordNi      = "ni,"
-		keywordIdle    = " id"
-	)
-
-	cpuIndex := strings.Index(topOutput, keywordCPULoad)
-	if cpuIndex == -1 {
-		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", keywordCPULoad)
-	}
-	topOutput = topOutput[cpuIndex+len(keywordCPULoad):]
-
-	newLineIndex := strings.Index(topOutput, keyWordNewLine)
-	if newLineIndex == -1 {
-		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", keyWordNewLine)
-	}
-	topOutput = topOutput[:newLineIndex]
-	cpuOutput := topOutput
-
-	sysIndex := strings.Index(topOutput, keywordNi)
-	if cpuIndex == -1 {
-		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", keywordNi)
-	}
-	topOutput = topOutput[sysIndex+len(keywordNi):]
-
-	idleIndex := strings.Index(topOutput, keywordIdle)
-	if idleIndex == -1 {
-		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", keywordIdle)
-	}
-	topOutput = strings.TrimSpace(topOutput[:idleIndex])
-
-	float, err := strconv.ParseFloat(topOutput, 32)
-	if err != nil {
-		return -1, fmt.Errorf("failed to parse float from %q taken out of %q", topOutput, cpuOutput)
-	}
-	return float32(100 - float), nil
+	return parseTopOutput(&parseTopOutputParams{
+		topOutput:         topOutput,
+		keywordCPULoad:    "%Cpu(s):",
+		keywordBeforeIdle: "ni,",
+		keywordIdle:       " id",
+	})
 }
--- cpu_reader.go
+++ cpu_reader.go
@@ -2,21 +2,87 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
-// cpuReader can read the CPU load using the OS API
-type cpuReader interface {
-	read() (float32, error)
-}
+const (
+	topCmd         = "top"
+	keyWordNewLine = "\n"
+)
+
+type (
+	// cpuReader can read the CPU load using the OS API
+	cpuReader func() (float32, error)
+
+	// parseTopOutputParams holds the parameters passed in to parse a top command's output
+	parseTopOutputParams struct {
+		topOutput, keywordCPULoad, keywordBeforeIdle, keywordIdle string
+	}
+)
+
+var topPath string
 
 // newCPUReader returns a CPU reader for the current OS if it is supported
 func newCPUReader() (cpuReader, error) {
+	var err error
+	topPath, err = loadTopPath()
+	if err != nil {
+		return nil, err
+	}
+
 	switch runtime.GOOS {
 	case "darwin":
-		return newOSXReader()
+		return readOSXCPULoad, nil
 	case "linux":
-		return newLinuxReader()
+		return readLinuxCPULoad, nil
 	}
 	return nil, fmt.Errorf("OS %q is not currently supported. No method of finding CPU load", runtime.GOOS)
+}
+
+func loadTopPath() (string, error) {
+	topPath, err := exec.LookPath(topCmd)
+	if err != nil {
+		return "", fmt.Errorf("Failed finding top path. Error : %s", err)
+	}
+	return topPath, nil
+}
+
+// parseTopOutput parses the cpu load out of the output of the top command in osx and linux
+// The load is calculated by finding the idle percentage and then subtracting it from 100
+// some load in both the system and usage percentages  hence it's easier just to parse the idle
+// time and then use it to calculate the busy percentage
+func parseTopOutput(params *parseTopOutputParams) (float32, error) {
+	cpuIndex := strings.Index(params.topOutput, params.keywordCPULoad)
+	if cpuIndex == -1 {
+		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", params.keywordCPULoad)
+	}
+	params.topOutput = params.topOutput[cpuIndex+len(params.keywordCPULoad):]
+
+	newLineIndex := strings.Index(params.topOutput, keyWordNewLine)
+	if newLineIndex == -1 {
+		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", keyWordNewLine)
+	}
+	params.topOutput = params.topOutput[:newLineIndex]
+	cpuOutput := params.topOutput
+
+	sysIndex := strings.Index(params.topOutput, params.keywordBeforeIdle)
+	if cpuIndex == -1 {
+		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", params.keywordBeforeIdle)
+	}
+	params.topOutput = params.topOutput[sysIndex+len(params.keywordBeforeIdle):]
+
+	idleIndex := strings.Index(params.topOutput, params.keywordIdle)
+	if idleIndex == -1 {
+		return -1, fmt.Errorf("failed to parse CPU load. Keyword %q was not found in top command's output", params.keywordIdle)
+	}
+	params.topOutput = strings.TrimSpace(params.topOutput[:idleIndex])
+
+	float, err := strconv.ParseFloat(params.topOutput, 32)
+	if err != nil {
+		return -1, fmt.Errorf("failed to parse float from %q taken out of %q", params.topOutput, cpuOutput)
+	}
+	return float32(100 - float), nil
 }
